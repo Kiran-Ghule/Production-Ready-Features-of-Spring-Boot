1fb33f5d3d96f5f3121b381c659fe4b2884dbbb1
Kiran-Ghule <ghulekiran1608@gmail.com>
2026-06-25 09:06:09 +0530

    Project Initiated

---
+++ src/test/java/com/week4/production_Ready/ProductionReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week4.production_Ready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionReadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
