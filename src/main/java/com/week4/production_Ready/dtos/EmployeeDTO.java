4b140a4eecc5cccde17da9f7898cb68f3fdcc2d1
Kiran-Ghule <ghulekiran1608@gmail.com>
2026-06-26 15:08:50 +0530

    EmployeeDTO is Added

--- src/main/java/com/week4/production_Ready/dtos/EmployeeDTO.java
+++ src/main/java/com/week4/production_Ready/dtos/EmployeeDTO.java
@@ -1,4 +1,27 @@
 package com.week4.production_Ready.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDTO {
+
+
+
+        private Long id;
+
+        private String name;
+
+        private String email;
+
+        private String role;
+
+        private Boolean isActive;
+
+        private Long Salary;
+
+
 }
