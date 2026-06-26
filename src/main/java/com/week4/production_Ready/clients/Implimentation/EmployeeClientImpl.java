9c99dadb8e1b4349ee19bbaa512f2a548f8ac8cd
Kiran-Ghule <ghulekiran1608@gmail.com>
2026-06-26 15:13:15 +0530

    RestClient build for Employee and Service methods and their implementation, Bean Creation are All Done

--- src/main/java/com/week4/production_Ready/clients/Implimentation/EmployeeClientImpl.java
+++ src/main/java/com/week4/production_Ready/clients/Implimentation/EmployeeClientImpl.java
@@ -1,4 +1,62 @@
 package com.week4.production_Ready.clients.Implimentation;
 
-public class EmployeeClientImpl {
+import com.week4.production_Ready.Advices.ResponseApi;
+import com.week4.production_Ready.clients.EmployeeClient;
+import com.week4.production_Ready.dtos.EmployeeDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class EmployeeClientImpl implements EmployeeClient {
+
+    private final RestClient restClient;
+
+    @Override
+    public List<EmployeeDTO> getAllEmployee() {
+        try {
+            ResponseApi<List<EmployeeDTO>> employees = restClient.get()
+                    .uri("/MyEmployees/Get/All")
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                    });
+            return employees.getData();
+        }
+        catch(Exception e)
+        {
+           throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public EmployeeDTO getEmployeeById(Long empid) {
+        try {
+            ResponseApi<EmployeeDTO> employee = restClient.get()
+                    .uri("/MyEmployees/Get/{empid}", empid)
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                    });
+
+            return employee.getData();
+        }
+        catch(Exception e)
+            {
+            throw new RuntimeException(e);
+            }
+    }
+
+    @Override
+    public EmployeeDTO createEmployee(EmployeeDTO employee) {
+        ResponseApi<EmployeeDTO> employeedto = restClient.post()
+                .uri("/MyEmployees/Post")
+                .body(employee)
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {});
+
+        return employeedto.getData();
+    }
 }
--- src/main/java/com/week4/production_Ready/configs/RestClientConfig.java
+++ src/main/java/com/week4/production_Ready/configs/RestClientConfig.java
@@ -1,4 +1,26 @@
 package com.week4.production_Ready.configs;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+import static org.apache.tomcat.util.http.fileupload.FileUploadBase.CONTENT_TYPE;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@Configuration
 public class RestClientConfig {
+
+    @Value("${employeeservice.base.url}")
+    private  String baseUrl;
+
+    @Bean
+    @Qualifier("EmployeeRestClient")
+    public RestClient getEmployeeRestClient() {
+        return  RestClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .build();
+    }
 }
